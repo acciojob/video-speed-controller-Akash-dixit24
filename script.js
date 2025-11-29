0517cbcfebc823f42adaec0013cd0ba88724afce
Akash Dixit <137980846+Akash-dixit24@users.noreply.github.com>
2025-11-29 17:41:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const inputs = document.querySelectorAll('.controls input');
+const speed = document.querySelector('.speed');
+const bar = document.querySelector('.speed-bar');
+const video = document.querySelector('video');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+speed.addEventListener('mousemove', function (e) {
+  const y = e.pageY - this.offsetTop;
+  const percent = y / this.offsetHeight;
+  
+  const min = 0.4;   // minimum speed
+  const max = 4;     // maximum speed
+  const playbackRate = percent * (max - min) + min;
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+  bar.style.height = `${percent * 100}%`;
+  bar.textContent = `${playbackRate.toFixed(2)}×`;
+
+  video.playbackRate = playbackRate;
+});
