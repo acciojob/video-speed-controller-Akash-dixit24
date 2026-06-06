54c787b2035478757841ca9d8e67cef14797520e
Akash Dixit <137980846+Akash-dixit24@users.noreply.github.com>
2026-06-06 20:58:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
-const speed = document.querySelector('.speed');
-const bar = document.querySelector('.speed-bar');
-const video = document.querySelector('video');
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressFilled = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const sliders = player.querySelectorAll(".player__slider");
+const skipButtons = player.querySelectorAll("[data-skip]");
 
-function handleMove(e) {
-    const y = e.offsetY;
-    const percent = y / speed.offsetHeight;
-
-    const min = 0.4;
-    const max = 4;
-
-    const height = Math.round(percent * 100) + '%';
-    const playbackRate = percent * (max - min) + min;
-
-    bar.style.height = height;
-    bar.textContent = playbackRate.toFixed(2) + '×';
-
-    video.playbackRate = playbackRate;
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
 }
 
-speed.addEventListener('mousemove', handleMove);
+function updateButton() {
+    toggle.textContent = video.paused ? "►" : "❚ ❚";
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressFilled.style.width = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime =
+        (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+
+toggle.addEventListener("click", togglePlay);
+
+sliders.forEach(slider =>
+    slider.addEventListener("input", handleRangeUpdate)
+);
+
+skipButtons.forEach(button =>
+    button.addEventListener("click", skip)
+);
+
+video.addEventListener("timeupdate", handleProgress);
+
+progress.addEventListener("click", scrub);
